f0ecf96993b4d428c79dc5d7c9010de03d8e7854
laheravilla <laheravilla@gmail.com>
2019-07-13 14:33:39 +0200

    Form age rane front and back-end

--- app-medical-diagnosis/src/Entity/User.php
+++ app-medical-diagnosis/src/Entity/User.php
@@ -44,6 +44,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Champs requis")
      */
     private $ageRange;
 
--- app-medical-diagnosis/src/Form/UserType.php
+++ app-medical-diagnosis/src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,7 +20,18 @@ class UserType extends AbstractType
             ->add('lastName', TextType::class, [
                 'label' => false
             ])
-//            ->add('ageRange')
+            ->add('ageRange', ChoiceType::class, [
+                'label' => false,
+                'expanded' => true,
+                'choices' => [
+                    '15 à 17 ans' => true,
+                    '18 à 24 ans' => true,
+                    '25 à 34 ans' => true,
+                    '35 à 49 ans' => true,
+                    '50 à 64 ans' => true,
+                    '+65 ans' => true,
+                ]
+            ])
 //            ->add('gender')
 //            ->add('socialStatus')
 //            ->add('numberOfChildren')
